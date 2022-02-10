f37df58d969fe21156a1170dfe1afa6d1bc75dbd
chattera@oregonstate.edu <amritachatterjee96@gmail.com>
2022-02-10 09:16:25 -0800

    Updated output page

--- AID.py
+++ AID.py
@@ -73,32 +73,35 @@ def MainProcess(usecase, subgoal, action, filename, var):
     # print the keywords S and A if violated
     result_1_S = C.checkRule1(keywords_S, txt)
     result_1_A = C.checkRule1(keywords_A, txt)
+    report = report + "\nRule 1: Keywords from subgoals and associated actions should be present on the webpage. \nThe wording of the subgoal serves as the information that Abi seeks, and the words from actions serve as cues to direct Abi to a UI action. Without such cues, Abi would face difficulty finding all the information they need. \n"
     if (result_1_S==1 and result_1_A==1) or (keywords_A == [] and keywords_S == []):
         report = report + "\nRule 1 not violated.\n"
     else:
-        report = report + "\nRule 1 violated: Keywords not found on the webpage.\n"
-        report = report + f"\nSubgoal keywords: {keywords_S}, Action Keywords: {keywords_A}\n"
+        report = report + "\nRule 1 is violated: Some keywords Abi was looking for, were not found on the webpage.\n"
+        report = report + f"\nThe subgoal keywords for this instance were: {keywords_S}, and the action keywords were: {keywords_A}.\n"
 
     print("Rule 1")
 
+    report = report+"\nRule 2: Linked pages should contain keywords from link labels. \n On clicking a link, the destination page should offer cues to help Abi’s understand that they have reached the right place. If a project page fails to use words similar to what a link label hinted at, Abi could get confused. \n"
     if (var==2):
         result_2 = C.checkRule2(filename, keywords_A)
         if result_2==1:
-            report = report + "\n Rule 2 is violated: Keywords from link-label is not present on the current page.\n"
+            report = report + "\n Rule 2 is violated: Keywords from the previous link-label is not present on the current page.\n"
         else:
             report = report + "\n Rule 2 not violated.\n"
     else:
-        report = report + "\n Rule 2 not applicable.\n"
+        report = report + "\n Rule 2 not applicable since it is a before-action webpage.\n"
 
     # #Rule 3 starts here - Link label exists or not
     # Highlight links which donot have label
     # Link labels will be checked
     document, result_3 = LinkParser(filename)
     print(filename)
+    report = report + "\nRule 3: Links should be labeled with a keyword or phrase. Abi clicks on a link only after gathering enough information and planning their next step. Labeled links provide Abi with information about the webpage they are supposed to visit.\n"
     if result_3 == -1:
-        report = report + "No Links Found"
+        report = report + "\nNo Links Found on the webpage.\n"
     elif len(result_3) > 0:
-        report = report + "\nRule 3 violated: Link is not labelled. Click on Results to view highlighted errors\n"
+        report = report + "\nRule 3 violated: Links are not labelled. Please refer to the right side to see the highlighted links (in yellow).\n"
     else:
         report= report + "\nRule 3 not violated. Results show the input html in this case.\n"
     print("Rule 3")
